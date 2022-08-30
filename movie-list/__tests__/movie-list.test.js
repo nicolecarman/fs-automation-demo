7213cc5848ed68ee95bd133fb2e2a7b03c445cfa
NicoleCarman <nicolecarman90@outlook.com>
2022-08-30 14:06:07 -0500

    Initial commit

---
+++ movie-list/__tests__/movie-list.test.js
@@ -0,0 +1,23 @@
+jest.setTimeout(10000)
+
+const { Builder, Capabilities } = require("selenium-webdriver")
+
+require('chromedriver')
+
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+
+const { addMovie } = require('./addMovie')
+
+beforeAll(async () => {
+    await driver.get('http://127.0.0.1:5500/movie-list/index.html')
+})
+
+afterAll(async () => {
+    await driver.quit()
+})
+
+test('Add a movie', async () => {
+    await addMovie(driver)
+
+    await driver.sleep(5000)
+})
